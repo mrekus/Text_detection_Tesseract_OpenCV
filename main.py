3b711a324b7918aeba50d52198b98e479638c798
Marius R <marius.rekus@gmail.com>
2022-11-29 13:16:10 +0200

    Supaprastinta

--- main.py
+++ main.py
@@ -13,22 +13,19 @@ if not cap.isOpened():
 if not cap.isOpened():
     raise IOError("Cannot open camera")
 
-offset = 0
 while True:
     ret, frame = cap.read()
-    offset += 1
-    if offset % 10 == 0:
-        imgW, imgH, _ = frame.shape
-        x1, y1, w1, h1 = 0, 0, imgW, imgH
+    imgW, imgH, _ = frame.shape
+    x1, y1, w1, h1 = 0, 0, imgW, imgH
 
-        img2char = pytesseract.image_to_string(frame)
-        img_boxes = pytesseract.image_to_boxes(frame)
-        for box in img_boxes.splitlines():
-            dimensions = box.split()
-            x, y, w, h = int(dimensions[1]), int(dimensions[2]), int(dimensions[3]), int(dimensions[4])
-            cv2.rectangle(frame, (x, imgW - y), (w, imgW - h), (255, 0, 0), 2)
+    img2char = pytesseract.image_to_string(frame)
+    img_boxes = pytesseract.image_to_boxes(frame)
+    for box in img_boxes.splitlines():
+        dimensions = box.split()
+        x, y, w, h = int(dimensions[1]), int(dimensions[2]), int(dimensions[3]), int(dimensions[4])
+        cv2.rectangle(frame, (x, imgW - y), (w, imgW - h), (255, 0, 0), 2)
 
-        cv2.putText(frame, img2char, (x1 + int(w1/50), y1 + int(h1/30)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(frame, img2char, (x1 + int(w1/50), y1 + int(h1/30)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
     cv2.imshow("Camera feed", frame)
 
